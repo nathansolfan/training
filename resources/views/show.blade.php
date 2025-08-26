340b8c800c76b1e9d147e6cfd6fd24f257067e3b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-08-26 23:31:09 +0100

    update

---
+++ resources/views/show.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+      <div>
+            <form action="/form" method="post">
+                @csrf
+                
+                
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" >
+
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" >    
+                
+                <label for="password">Password</label>
+                <input type="text" name="password" id="password" >
+
+                <button type="submit">Click</button>
+            </form>
+        </div>
+
+@foreach ( as )
+    
+@endforeach
+    
+</body>
+</html>
